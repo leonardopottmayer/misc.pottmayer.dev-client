8f91de4a968c5e27d9f8b2f583213adb30ab1f07
leonardopottmayer <pottmayerleonardo@gmail.com>
2022-12-03 21:14:45 -0300

    Created layout component around app

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+import styles from "./Layout.module.css";
+
+const Layout = ({ children }: any) => {
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta charSet="UTF-8" />
+        <meta name="description" content="pottmayer.dev random stuff" />
+        <meta
+          name="keywords"
+          content="Leonardo, Pottmayer, Leonardo Pottmayer, pottmayer.dev"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <title>pottmayer.dev</title>
+      </Head>
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
