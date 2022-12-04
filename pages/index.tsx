f9c6c3c49cee7905d9f6ddb02c884b85791f6b00
leonardopottmayer <pottmayerleonardo@gmail.com>
2022-12-03 22:03:17 -0300

    Added github profile image

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,23 +6,23 @@ import "animate.css";
 export default function HomePage() {
   return (
     <div className={styles.page}>
-      <div className={styles.title_container}>
+      <div
+        className={`${styles.title_container} animate__animated animate__fadeIn`}
+      >
+        <Image src={"/favicon.ico"} width={"30"} height={"30"} alt={"Logo"} />
+        <div className={styles.portfolio_link}>
+          <Link href={"https://pottmayer.dev"} target={"_blank"}>
+            <h1>pottmayer.dev</h1>
+          </Link>
+        </div>
+
         <Image
-          className={"animate__animated animate__fadeIn"}
-          src={"/favicon.ico"}
-          width={"30"}
-          height={"30"}
-          alt={"Logo"}
+          className={styles.github_profile_image}
+          src={"https://avatars.githubusercontent.com/u/50502129?v=4"}
+          width={"150"}
+          height={"150"}
+          alt={"Github profile image"}
         />
-        <Link
-          className={styles.portfolio_link}
-          href={"https://pottmayer.dev"}
-          target={"_blank"}
-        >
-          <h1 className={"animate__animated animate__fadeInUp"}>
-            pottmayer.dev
-          </h1>
-        </Link>
       </div>
     </div>
   );
